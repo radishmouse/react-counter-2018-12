a24e86b406c6bafdef81c1bfc9a88ffcc8c00da1
Chris Aquino <chris.aquino@gmail.com>
2018-12-19 14:27:42 -0500

    adds fetch example with pagination

--- src/App.js
+++ src/App.js
@@ -3,25 +3,91 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageNumber: 0,
+      pokemonToShow: [],
+      allPokemon: [
+        {
+          url: 'http://pokemon/1'
+        },
+        {
+          url: 'http://pokemon/2'
+        },
+        {
+          url: 'http://pokemon/3'
+        },
+        {
+          url: 'http://pokemon/4'
+        },
+        {
+          url: 'http://pokemon/5'
+        },
+
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>{this.state.pageNumber}</div>
+        <button onClick={this._prevPage}>-</button>
+        <button onClick={this._nextPage}>+</button>
+
+        <ul>
+          {this.state.pokemonToShow.map(pokemon => {
+            return <li>pokemon.name</li>
+          })}
+        </ul>
       </div>
     );
+  }
+
+  _getPokemonForPage = () => {
+
+    this.setState({
+      pokemonToShow: []
+    }, () => {
+      const from = this.state.pageNumber;
+      const to = this.state.pageNumber + 2;
+  
+      for (let i = from; i<to; i++) {
+        console.log(`doing a fetch for: ${this.state.allPokemon[i].url}`);
+        /*
+          fetch(this.state.allPokemon[i].url)
+            .then(pokemon => {
+              this.setState({
+                pokemonToShow: [...this.state.pokemonToShow, pokemon]
+              })
+            })
+  
+        */
+  
+      }
+    })
+
+
+
+  }
+
+  _nextPage = () => {
+    this.setState({
+      pageNumber: this.state.pageNumber + 1
+    }, () => {
+      // pretend that i'm going to fetch
+      this._getPokemonForPage();
+    });
+  }
+  _prevPage = () => {
+    this.setState({
+      pageNumber: this.state.pageNumber - 1
+    }, () => {
+      // pretend that i'm going to fetch
+      this._getPokemonForPage();
+    });
   }
 }
 
